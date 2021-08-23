d189fa220d05d9f18fc06b3cc092feb972ea67f0
kumax0 <Canerunus@gmail.com>
2021-08-23 12:29:12 +0200

    added class dateprofile

--- src/game.js
+++ src/game.js
@@ -1,1 +1,35 @@
 // GAME FUNCTIONALITY
+
+// Customizable dateprofile skeletton
+class Dateprofile {
+
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    bio() {
+        let bio = "put description here";
+        return bio;
+    }
+
+    greeting() {
+        let greeting = "put greeting here";
+        return greeting;
+    }
+}
+// Date profile number 1
+
+const date1 = new Dateprofile('peter', 23)
+
+//  //Test logs of Date profile number 1
+
+// console.log(date1.bio())
+// console.log(date1.name)
+// console.log(date1.age)
+
+// date1.bio = 'hi this is my new bio'
+
+// console.log(date1.bio)
+// console.log(date1.greeting())
+
