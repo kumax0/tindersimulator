95bd8aab02b1f4db20b2616678ff71483e287d3d
kumax0 <Canerunus@gmail.com>
2021-08-24 16:09:09 +0200

    Created like function -> opening chat window

--- src/game.js
+++ src/game.js
@@ -25,8 +25,15 @@ const card2 = new Dateprofile('Alice', 99);
 const card3 = new Dateprofile('Blümchen', 44);
 const cardArr = [card1, card2, card3];
 let currentCardIndex = 0
-//console.log(cardArr)
-// MATCH SYSTEM: Card swipe mechanics --> iterate through array on click
+//  //Test logs of date profile number 1
+// console.log(card1.bio())
+// console.log(card1.name)
+// console.log(card1.age)
+// date1.bio = 'hi this is my new bio'
+// console.log(card1.bio)
+// console.log(card1.greeting())
+
+// MATCH SYSTEM: Card swipe mechanics --> iterate through array on notLike button
 function notLike() {
     // Creater Iterator that increments on button click
     let currentCard = cardArr[currentCardIndex++]
@@ -38,19 +45,18 @@ function notLike() {
         currentCardIndex = 0
     }
 }
-
+   // MATCH SYSTEM: Open chat window on like button
 function like() {
-    // When Like is clicked, it should open up the chat window with that profile
-    
+    // When like is clicked, turn chatwindow display on.
+    document.getElementById("chat-window").style.display = "block";
+    // when chatwindow is on, turn off matching system
+    document.querySelector("body > section.cards > p").style.display = "none"
+}
+// Reset
+function reset() {
+    location.reload()
 }
 
-//  //Test logs of date profile number 1
-// console.log(card1.bio())
-// console.log(card1.name)
-// console.log(card1.age)
-// date1.bio = 'hi this is my new bio'
-// console.log(card1.bio)
-// console.log(card1.greeting())
 
 
 // // Not like button
@@ -63,4 +69,7 @@ window.addEventListener('load', () => {
 
     const likeBtn = document.getElementById('like');
     likeBtn.addEventListener('click', like);
+
+    const resetBtn = document.getElementById('reset');
+    resetBtn.addEventListener('click', reset);
   });
